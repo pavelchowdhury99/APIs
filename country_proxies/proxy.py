a515306bf8ae2ffe5f350bc27b487d125888c08c
pavelchowdhury99 <59216690+pavelchowdhury99@users.noreply.github.com>
2021-08-08 22:52:36 +0530

    Uploading initial skeleton.

---
+++ country_proxies/proxy.py
@@ -0,0 +1,11 @@
+from common import read_yaml
+import requests
+import pandas as pd
+import yaml
+import os
+from bs4 import BeautifulSoup
+
+class Proxy():
+    pass
+
+print(read_yaml('config.yaml'))
