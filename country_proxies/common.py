b03eb9f96c976ca00011050aec3101d2e7c69367
pavelchowdhury99 <pavelchandra99@gmail.com>
2021-08-09 13:43:47 +0530

    Adding us proxy scrapping method, check_proxy function, update proxy skeleton and required files.

--- country_proxies/common.py
+++ country_proxies/common.py
@@ -1,4 +1,6 @@
 import yaml
+import logging
+
 
 def read_yaml(file_path):
     """
@@ -6,3 +8,21 @@ def read_yaml(file_path):
     """
     with open(file_path) as f:
         return yaml.safe_load(f)
+
+
+def get_logger():
+    """
+    Return a logger object
+    """
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter("%(asctime)s - %(message)s")
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+    return logger
--- country_proxies/proxy.py
+++ country_proxies/proxy.py
@@ -1,9 +1,15 @@
-from country_proxies.common import read_yaml
+from country_proxies.common import read_yaml, get_logger
+import sys
+from timeit import timeit
 import requests
 import pandas as pd
 import yaml
 import os
 from bs4 import BeautifulSoup
+import logging
+import json
+
+logger = get_logger()
 
 
 class Proxy:
@@ -12,22 +18,62 @@ class Proxy:
         self.config = read_yaml(config_yaml_path)['PROXY_WEBSITES'][country_code.upper()]
         self.context = context
 
+    @staticmethod
+    def check_proxy_workings(proxy_dict, url_to_check):
+        """
+        Static function to check if a proxy is working or not
+        Returns True/False
+        """
+        try:
+            if requests.get(url_to_check, proxies=proxy_dict, timeout=200).ok:
+                return True
+        except:
+            return False
+
+    def update_proxies(self):
+        with open(read_yaml('config.yaml')['PROXY_DATAFRAME']['PATH']) as f:
+            d = json.load(f)
+            print(d)
+
     def extract_proxies(self):
-        '''
+        """
         Fetch proxies from website and return a dictionary
-        This fucntion needs to be overwritten country to country
-        '''
+        This function needs to be overwritten country to country
+        """
         # reading website for proxies
         proxy_website = self.config.get('PROXY_SITE')
-        print(proxy_website)
+
         # reading page table into DataFrame
-        df = pd.read_html(requests.get(proxy_website, timeout=200).text)
-        http_test_site = self.config.get('HTTP_TEST')
-        https_test_site = self.config.get('HTTPS_TEST')
-        print(http_test_site, https_test_site)
+        df = pd.read_html(requests.get(proxy_website, timeout=200).text)[0]
+
+        # separating http and https proxies
+        df_http = df[df['Https'] == 'no']
+        df_https = df[df['Https'] == 'yes']
+        proxy_dict = {}
+
+        # fetching proxy for http url
+        http_test_url = self.config.get("HTTP_TEST")
+        for count, proxy in enumerate(df_http.iterrows()):
+            proxy_dict['http'] = f"http://{proxy[1]['IP Address']}:{int(proxy[1]['Port'])}"
+            if Proxy.check_proxy_workings(proxy_dict=proxy_dict, url_to_check=http_test_url):
+                logger.info(f'Proxy {proxy_dict} has worked')
+                break
+            else:
+                logger.info(f'Proxy {count} failed')
+
+        # # fetching proxy for http url
+        # https_test_url = self.config.get("HTTPS_TEST")
+        # for count, proxy in enumerate(df_https.iterrows()):
+        #     proxy_dict['https'] = f"https://{proxy[1]['IP Address']}:{int(proxy[1]['Port'])}"
+        #     if Proxy.check_proxy_workings(proxy_dict=proxy_dict, url_to_check=https_test_url):
+        #         logger.info(f'Proxy {proxy_dict} has worked')
+        #         break
+        #     else:
+        #         logger.info(f'Proxy {count} failed {proxy_dict}')
 
 
 if __name__ == "__main__":
     x = Proxy(country_code='us', config_yaml_path='config.yaml', context={})
-    x.extract_proxies()
+    # x.extract_proxies()
+    x.update_proxies()
     # print(x.context)
