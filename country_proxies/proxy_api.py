215c682705faff7efc4b1bbea00fc0f0a53f4d88
pavelchowdhury99 <pavelchandra99@gmail.com>
2021-08-10 22:06:50 +0530

    Adding multiprocessing in app to update proxies and run app simultaneously

--- country_proxies/proxy_api.py
+++ country_proxies/proxy_api.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
+from proxy import Proxy
+import multiprocessing
 import sqlite3
+import time
+import yaml
+import random
 
 app = Flask(__name__)
+
+
+def activate():
+    print("Inside activate")
+    while 1:
+        conn = sqlite3.connect("Proxy.db")
+        cursor = conn.cursor()
+        x = Proxy(country_code='us', config_yaml_path='config.yaml')
+        x.run(context={'cursor': cursor})
+        # x.update_proxies(proxies={'http': 'http://98.116.152.143:3128', 'https': 'http://20.69.69.212:3128'}, context={'cursor': cursor})
+        # print(x.get_country_proxies_from_db(context={'cursor': cursor}))
+        # x.extract_new_proxies()
+        conn.commit()
+        conn.close()
+        time.sleep(15)
 
 
 @app.route('/')
@@ -13,11 +33,22 @@ def home():
                     WHERE COUNTRY='US';
                     ''')
     result = cursor.fetchall()[0]
-    response = {result[0]: {'http': result[1], 'https': result[2]}}
-    conn.commit()
+    with open('config.yaml') as f:
+        list_ = yaml.safe_load(f).get('USER_AGENTS')
+    response = {result[0]: {'http': result[1], 'https': result[2]}, 'RANDOM_USER_AGENT': random.choice(list_), 'USER_AGENTS':list_}
+    # conn.commit()
     conn.close()
-    return response
+    return jsonify(response)
+
+def API():
+    print('In API')
+    app.run(debug=True)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # p = multiprocessing.Process(target=API, args=())
+    p = multiprocessing.Process(target=API)
+    p.start()
+    time.sleep(3)
+    print('After Flask run')
+    activate()
--- country_proxies/temp.py
+++ country_proxies/temp.py
@@ -29,14 +29,49 @@ import sqlite3
 # conn.close()
 
 # # Selecting from table
-conn = sqlite3.connect("Proxy.db")
-cursor = conn.cursor()
-cursor.execute('''SELECT COUNTRY,HTTP, HTTPS
-                FROM PROXY
-                WHERE COUNTRY='US';
-                ''')
-result = cursor.fetchall()[0]
-response = {result[0]: {'http': result[1], 'https': result[2]}}
-print(response)
-conn.commit()
-conn.close()
+# conn = sqlite3.connect("Proxy.db")
+# cursor = conn.cursor()
+# cursor.execute('''SELECT COUNTRY,HTTP, HTTPS
+#                 FROM PROXY
+#                 WHERE COUNTRY='US';
+#                 ''')
+# result = cursor.fetchall()[0]
+# response = {result[0]: {'http': result[1], 'https': result[2]}}
+# print(response)
+# conn.commit()
+# conn.close()
+
+
+# import flask
+# from flask import request, jsonify, make_response
+# import time
+# import multiprocessing
+#
+# app = flask.Flask('__name__')
+#
+#
+# @app.route('/')
+# def home():
+#     return ({"home": 'home'})
+#
+#
+# def API(Conf):
+#     print('In API selction')
+#     app.run(debug=True)
+#
+#
+# if __name__ == "__main__":
+#     config = {"Something": "SomethingElese"}
+#     p = multiprocessing.Process(target=API, args=('a',))
+#     p.start()
+#     time.sleep(3)
+#     print('After Flask run')
+
+# import yaml
+# with open('config.yaml') as f:
+#     dict_=yaml.safe_load(f).get('USER_AGENTS')
+#     print(dict_)
+
+import requests
+d = {'http': None, 'https': 'http://34.138.225.120:8888'}
+print(requests.get('http://www.ritba.org/tolls-2/', proxies=d).text)
